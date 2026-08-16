0d00e2467036570ab0033bfaf39eeef6a6d10a50
jiopra-dev <jiopran8n@gmail.com>
2026-08-16 01:05:58 -0300

    Update next.config.ts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,17 +2,32 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'standalone',
-  eslint: { ignoreDuringBuilds: true },
-  typescript: { ignoreBuildErrors: false },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: false,
+  },
   experimental: {
     serverActions: {
-      bodySizeLimit: "500mb", // ESSENCIAL para upload de vídeos
+      bodySizeLimit: "500mb",
     },
   },
   async rewrites() {
     return [
-      { source: "/js/script.js", destination: "https://datafa.st/js/script.js" },
-      { source: "/api/events", destination: "https://datafa.st/api/events" },
+      {
+        source: "/js/script.js",
+        destination: "https://datafa.st/js/script.js",
+      },
+      {
+        source: "/api/events",
+        destination: "https://datafa.st/api/events",
+      },
+      // 👇 ESSA É A MÁGICA: Redireciona o upload direto para o Backend Python!
+      {
+        source: "/api-backend/:path*",
+        destination: "http://backend:8000/:path*",
+      },
     ];
   },
 };
